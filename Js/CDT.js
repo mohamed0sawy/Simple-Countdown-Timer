3175f7aed1b97706c8534c6892e85b16564c296c
mohamed0tarek <92942683+mohamed0tarek@users.noreply.github.com>
2022-05-03 10:38:09 +0200

    Add files via upload

---
+++ Js/CDT.js
@@ -0,0 +1,22 @@
+let seekingDate = new Date("Dec 31, 2022 23:59:59").getTime();
+console.log(seekingDate);
+
+let counter = setInterval(function(){
+    let currentDate = new Date().getTime();
+    let diffDate = seekingDate - currentDate;
+
+    let days = Math.floor(diffDate / (1000 * 60 * 60 * 24))
+    let hours = Math.floor((diffDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutes = Math.floor((diffDate % (1000 * 60 * 60)) / (1000 * 60));
+    let seconds = Math.floor((diffDate % (1000 * 60)) / (1000));
+
+    document.querySelector(".days .nom").innerHTML = (days < 100 && days > 10) ? `0${days}`  :
+                                                    days < 10 ? `00${days}` : days ;
+    document.querySelector(".hours .nom").innerHTML = hours < 10 ? `0${hours}` : hours ;
+    document.querySelector(".minutes .nom").innerHTML = minutes < 10 ? `0${minutes}` : minutes ;
+    document.querySelector(".seconds .nom").innerHTML = seconds < 10 ? `0${seconds}` : seconds ;
+
+    if (diffDate < 0 ){
+        clearInterval(counter);
+    }
+},1000)
